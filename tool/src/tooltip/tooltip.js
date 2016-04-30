0570030877a98f93fe61690c387b1aaa589c1407
LinJieLinLin <993353454@qq.com>
2016-05-01 02:21:49 +0800

    修改gulp

---
+++ tool/src/tooltip/tooltip.js
@@ -0,0 +1,25 @@
+try {
+    if (typeof directive !== 'object') {
+        var directive = angular.module('directive', []);
+    }
+} catch (e) {
+    var directive = angular.module('directive', []);
+}
+directive.directive('tooltip', function($document) {
+    return {
+        templateUrl: '../../src/tooltip/tooltip.html',
+        restrict: 'A',
+        replace: true,
+        transclude: true,
+        scope: {
+            c: "=",
+        },
+        controller: function($scope, $element, $attrs) {
+            if ($scope.c) {
+                angular.forEach($scope.c, function(value, key) {
+                    $scope[key] = value;
+                });
+            }
+        }
+    };
+});
---
+++ tooltip/tooltip.js
@@ -0,0 +1,25 @@
+try {
+    if (typeof directive !== 'object') {
+        var directive = angular.module('directive', []);
+    }
+} catch (e) {
+    var directive = angular.module('directive', []);
+}
+directive.directive('tooltip', ['$document', function($document) {
+    return {
+        template:'',
+        restrict: 'A',
+        replace: true,
+        transclude: true,
+        scope: {
+            c: "=",
+        },
+        controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
+            if ($scope.c) {
+                angular.forEach($scope.c, function(value, key) {
+                    $scope[key] = value;
+                });
+            }
+        }]
+    };
+}]);
